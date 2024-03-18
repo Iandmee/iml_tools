5cea11cf7a096ca30fdcb297cf43733c11e490fb
Nikita Proskurnikov <nikita.proskurnikov@ejetbrains.com>
2024-03-18 15:04:42 +0100

    add external/skia modifications

--- setup_build_files.sh
+++ setup_build_files.sh
@@ -2,7 +2,7 @@
 chmod +x setup_variables.sh
 source ./setup_variables.sh
 ln -s ~/.m2 m2
-(cd tools/base && git apply ../../tools.base.patch)
+(cd external/skia && git apply ../../skia.patch)
 find $PROJECT_PATH -name "*.iml" -execdir rm  BUILD 2> /dev/null \;
 find $PROJECT_PATH -name "*.iml" -execdir touch  BUILD \;
 bazel run --action_env=BAZEL_CXXOPTS="-std=c++20" --java_runtime_version=remotejdk_17 @iml_tools//tools/base/bazel:iml_to_build -- --project_path $PROJECT_PATH
